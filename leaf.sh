1617bc570f54681c7bf9ff0762306e6090e5e72b
Wumbledumbledum <jaybuffay@aol.com>
2017-12-04 21:34:18 +0000

    The most important service rendered by the press is that of educating people to approach printed matter with distrust.

---
+++ leaf.sh
@@ -0,0 +1,4 @@
+#!/usr/local/bin/bash
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/home/acorn/bin
+sleep $[ ( $RANDOM % 59 ) + 1 ]
+cd /home/git/git-wall-colormaker/ && echo $(date) "deleriousity" | md5 > ./green_monster.vrb && git commit -a -m "$(fortune -s | sed 's/"//g')" && git push;
