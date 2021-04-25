074a96bd53b7f5bd46e6f9680dc5331c0212bf18
Peter Pan <peter@never.lan>
2021-04-25 18:25:57 +0000

    Abandon the search for Truth; settle for a good fantasy.

--- sprout.sh
+++ sprout.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/home/git/bin
 #sleep $[ ( $RANDOM % 59 ) + 1 ] //execute now//
 cd /home/git/git-wall-colormaker.git/ && echo $(date) "deleriousity" | md5sum > ./green_monster.vrb && git commit -a -m "$(fortune -s | sed 's/"//g')" && git push;
